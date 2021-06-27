6b5fcceafd88f6a28397dc32827f7855a4e4c53d
Ritesh Kumar <56651899+wordssaysalot@users.noreply.github.com>
2021-06-27 21:31:28 +0530

    Update Restrictive Candy Crush.cpp

--- cpp/Restrictive Candy Crush.cpp
+++ cpp/Restrictive Candy Crush.cpp
@@ -14,18 +14,78 @@
 // Modified String after each step: 
 // "geeksforgeeks" -> "gksforgks"
 
-Given a string s and an integer k, the task is to reduce the string by applying the following operation:
-Choose a group of k consecutive identical characters and remove them.
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
-The operation can be performed any number of times until it is no longer possible.
 
-Example 1:
+ // } Driver Code Ends
+class Solution{
+    public:
+    string Reduced_String(int k,string s){
+        // Your code goes here
+        
+        string ans;
+        stack<pair<char,int>> st;
+        
+        for(int i=0;i<s.length();i++){
+            if(st.empty()){
+                st.push({s[i],1});
+            }
+            else{
+                auto x=st.top();
+                st.pop();
+                x.second%=k;
+                
+                if(x.first==s[i]){
+                    st.push({x.first,x.second+1});
+                }
+                else{
+                    if(x.second>0)
+                        st.push(x);
+                    st.push({s[i],1});
+                }
+                
+                x=st.top();
+                st.pop();
+                x.second%=k;
+                if(x.second){
+                    st.push(x);
+                }
+                
+            }
+        }
+        
+        while(!st.empty()){
+            auto x=st.top();st.pop();
+            for(int i=0;i<x.second%k;i++){
+                ans+=x.first;
+            }
+        }
+        reverse(ans.begin(),ans.end());
+        
+        return ans;
+    }
 
-Input:
-k = 2
-s = "geeksforgeeks"
-Output:
-gksforgks
-Explanation:
-Modified String after each step: 
-"geeksforgeeks" -> "gksforgks"
+
+};
+
+// { Driver Code Starts.
+
+int main() {
+    
+    
+    int t;cin>>t;
+    while(t--)
+    {
+        int k;
+        cin>>k;
+        string s;
+        cin>>s;
+        Solution obj;
+        cout<<obj.Reduced_String(k,s)<<"\n";
+        
+    }
+    
+	return 0;
+}  // } Driver Code Ends
