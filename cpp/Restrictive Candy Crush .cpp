44c831461d5e57e8ac60ddb3760d238ac99a397b
Ritesh Kumar <56651899+wordssaysalot@users.noreply.github.com>
2021-06-28 14:39:24 +0530

    day 17 : gfg30daysofcode

---
+++ cpp/Restrictive Candy Crush .cpp
@@ -0,0 +1,19 @@
+//  Restrictive Candy Crush 
+//
+// Given a string s and an integer k, the task is to reduce the string by applying the following operation:
+// Choose a group of k consecutive identical characters and remove them.
+
+// The operation can be performed any number of times until it is no longer possible.
+
+// Example :
+
+// Input:
+// k = 2
+// s = "geeksforgeeks"
+// Output:
+// gksforgks
+// Explanation:
+// Modified String after each step: 
+// "geeksforgeeks" -> "gksforgks"
+
+
