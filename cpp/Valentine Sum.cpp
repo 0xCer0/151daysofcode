2cf956b3bee862ee86d6a2d9b882ca3af4e0b92f
Ritesh Kumar <1906652@kiit.ac.in>
2021-07-09 22:36:38 +0530

    day 19 : gfg30daysofcode

---
+++ cpp/Valentine Sum.cpp
@@ -0,0 +1,22 @@
+// Cupid wants to strike maximum houses in Geek Land on Valentine's Day. The houses in Geek Land are arranged in the form of a binary tree. Cupid is standing at target node initially. 
+// Find the sum of all nodes within a maximum distance k from target node. The target node should be included in the sum.
+
+
+// Example 1:
+
+// Input:
+//                    1
+//                  /    \
+//                 2      9
+//                /      /  \
+//               4      5     7
+//             /   \         /  \
+//            8     19     20    11
+//           /     /  \
+//          30   40   50
+// target = 9, K = 1
+// Output:
+// 22
+// Explanation:
+// Nodes within distance 1 from 9 are 9, 5, 7, 1 
+
