2c4308847d80dfefc0c60e5b96c05392861c9a0f
Ritesh Kumar <56651899+wordssaysalot@users.noreply.github.com>
2021-07-14 22:37:32 +0530

    day 21 : gfg30daysofcode

---
+++ cpp/Corona Vaccine.cpp
@@ -0,0 +1,25 @@
+// Geek has developed an effective vaccine for Corona virus and he wants each of the N houses in Geek Land to have access to it. Given a binary tree where each node represents a house in Geek Land, find the minimum number of houses that should be supplied with the vaccine kit if one vaccine kit is sufficient for that house, its parent house and it's immediate child nodes.  
+
+ 
+
+// Example :
+
+// Input:
+//     1
+//    / \
+//   2   3 
+//         \
+//          4
+//           \
+//            5
+//             \
+//              6
+
+// Output:
+// 2
+// Explanation:
+// The vaccine kits should be 
+// supplied to house numbers 1 and 5. 
+
+#include <bits/stdc++.h>
+using namespace std;
