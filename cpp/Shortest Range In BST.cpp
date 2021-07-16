ee5bf553a00e2fa4ea9e77a223b4d31778dc7721
Ritesh Kumar <56651899+wordssaysalot@users.noreply.github.com>
2021-07-16 21:21:58 +0530

    day 23 : gfg30daysofcode

---
+++ cpp/Shortest Range In BST.cpp
@@ -0,0 +1,20 @@
+Given a BST (Binary Search Tree), find the shortest range [x, y], such that, at least one node of every level of the BST lies in the range.
+If there are multiple ranges with the same gap (i.e. (y-x)) return the range with the smallest x.
+
+Example 1:
+
+Input:
+              8
+          /   \
+         3     10
+       /  \      \
+      2    6      14
+          / \     /
+         4   7   12
+                /  \
+               11   13
+Output: 6 11
+Explanation: Level order traversal of the tree 
+is [8], [3, 10], [2, 6, 14], [4, 7, 12], [11, 13]. 
+The shortest range which satisfies the above 
+mentioned condition is [6, 11]. 
