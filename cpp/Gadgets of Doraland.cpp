7f695ccf59f0e23d036eacb57d1c8ffef94760bd
Ritesh Kumar <56651899+wordssaysalot@users.noreply.github.com>
2021-07-30 20:19:21 +0530

    Day 28 : gfg30daysofcode

---
+++ cpp/Gadgets of Doraland.cpp
@@ -0,0 +1,13 @@
+// In Doraland, people have unique Identity Numbers called D-id. Doraemon owns the most popular gadget shop in Doraland. Since his gadgets are in high demand and he has only K gadgets left he has decided to sell his gadgets to his most frequent customers only. N customers visit his shop and D-id of each customer is given in an array array[ ]. In case two or more people have visited his shop the same number of time he gives priority to the customer with higher D-id. Find the D-ids of people he sells his K gadgets to.
+
+// Example :
+
+// Input:
+// N = 6
+// array[] = {1, 1, 1, 2, 2, 3}
+// K = 2
+// Output: 
+// 1 2
+// Explanation: 
+// Customers with D-id 1 and 2 are most 
+// frequent.
