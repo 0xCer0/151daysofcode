adff0e931345c574319265917a7b5a65facbf6f4
Ritesh Kumar <56651899+wordssaysalot@users.noreply.github.com>
2021-07-31 19:50:50 +0530

    Day 29 : gfg30daysofcode

--- cpp/Gadgets of Doraland.cpp
+++ cpp/Gadgets of Doraland.cpp
@@ -11,3 +11,73 @@
 // Explanation: 
 // Customers with D-id 1 and 2 are most 
 // frequent.
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+class Solution{
+    public:
+    vector<int> TopK(vector<int>& array, int k)
+    {
+        map<int ,int >mp;
+        
+        for (int i=0;i<array.size();i++)
+        {
+            mp[array[i]]++;
+        }
+        
+        vector<pair<int ,int >>v;
+        
+        for ( auto i:mp)
+        {
+            v.push_back({i.second,i.first});
+        }
+        
+        sort(v.begin(),v.end(),greater<pair<int ,int>>());
+        
+        vector<int >ans;
+        
+        for (auto i : v)
+        {
+            if (k>0)
+            {
+                ans.push_back(i.second);
+                k--;
+            }
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k ;
+        cin>>n;
+        
+        vector<int>array(n);
+        
+        for(int i=0; i<n; i++)
+            cin>>array[i];
+            
+        cin>>k;
+        
+        Solution obj;
+        vector<int> result = obj.TopK(array,k);
+        
+        for(int i=0; i<result.size();i++)
+            cout<<result[i]<<" ";
+
+        cout<<"\n";
+
+    }
+    return 0;
+}  // } Driver Code Ends
