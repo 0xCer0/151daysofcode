efd197d14cf22a6b2788145aa5ffa66d7362387d
Ritesh Kumar <56651899+wordssaysalot@users.noreply.github.com>
2021-08-17 15:37:43 +0530

    Day 34 : gfg30daysofcode

--- cpp/Water the plants.cpp
+++ cpp/Water the plants.cpp
@@ -12,3 +12,89 @@
 // can water thefull gallery, span of
 // sprinkler at index 2 = [0,4] and span
 // â€‹of sprinkler at index 5 = [5,5].
+
+// driver code
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    int min_sprinklers(int gallery[], int n)
+    {
+
+        vector<pair<int, int> > V;
+ 
+    for (int i = 0; i < n; i++) {
+        if (gallery[i] > -1) {
+            V.push_back(
+                pair<int, int>(i - gallery[i], i + gallery[i]));
+        }
+    }
+   
+    sort(V.begin(), V.end());
+ 
+    int maxRight = 0;
+   
+    int res = 0;
+ 
+    int i = 0;
+ 
+    while (maxRight < n) {
+
+ 
+        if (i == V.size() || V[i].first > maxRight) {
+            return -1;
+        }
+    
+        int currMax = V[i].second;
+ 
+ 
+        while (i + 1 < V.size()
+               && V[i + 1].first <= maxRight) {
+ 
+       
+            i++;
+  
+            currMax = max(currMax, V[i].second);
+        }
+ 
+        if (currMax < maxRight) {
+            // Return -1
+            return -1;
+        }
+        // Increment res by 1
+        res++;
+ 
+        // Update maxRight
+        maxRight = currMax + 1;
+ 
+        // Increment i by 1
+        i++;
+    }
+    // Return res as answer
+    return res;
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        
+        int gallery[n];
+        for(int i=0; i<n; i++)
+            cin>> gallery[i];
+        Solution obj;
+        cout<< obj.min_sprinklers(gallery,n) << endl;
+    }
+	return 1;
+}
+  // } Driver Code Ends
