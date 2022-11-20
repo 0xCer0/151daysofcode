5e7283240f6f4aac3a25e8c24e57f9f4df13d1b9
Ritesh Kumar <riteshkumar.js@protonmail.com>
2022-11-20 16:45:03 +0530

    Create TwoOutofThree.py

---
+++ py/leetcode/TwoOutofThree.py
@@ -0,0 +1,48 @@
+# https://leetcode.com/problems/two-out-of-three/
+
+# class Solution:
+#     def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
+
+# num1 = []        
+# for i in input(int()):
+#     num1.append(input(''))
+    
+# print(num1)
+
+# def solution(num1,num2,num3):
+
+# list1 = []
+# list2 = []
+# list3 = []
+
+# n1=int(input())
+# for i in range(0,n1):
+#     ele = int(input())
+#     list1.append(ele)
+
+# print(list1)
+
+# n2 = int(input())
+# for i in range(0,n2):
+#     list2.append(int(input()))
+    
+# print(list2)
+
+list1 = []
+list2 = []
+list3 = []
+
+n1=int(input())
+for i in range(0,n1):
+    ele = int(input())
+    list1.append(ele)
+
+print(list1)
+
+n2 = int(input())
+for i in range(0,n2):
+    list2.append(int(input()))
+    
+print(list2)
+    
+    
